040057007fc8b253b4ccfc89c55f11ab1ce7c043
Joshua Shor <jpshor@gmail.com>
2014-09-07 16:06:44 -0400

    renamed ZintTest to zint-csharp

--- ZintTest/Properties/Resources.Designer.cs
+++ ZintTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ZintTest.Properties {
+namespace ZintWrapper.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ZintTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ZintTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ZintWrapper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
